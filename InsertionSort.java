ae035f33e26273815d03ca817ab360238a122a2a
nethusa <hennayakenethusha@gmail.com>
2024-06-06 12:45:36 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,38 @@
+package Insertion;
+
+public class InsertionSort {
+ 
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; ++i) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+
+    public static void printArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+  
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6};
+        System.out.println("Original array:");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+}
